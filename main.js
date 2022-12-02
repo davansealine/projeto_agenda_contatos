797b21a0d6161a6c214c767ca934c800c9a5c1f1
davansealine <alinedavanse@gmail.com>
2022-12-02 20:35:05 -0300

    adicionando arquivos index.html, main.css, main.js e imagens

---
+++ main.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form-contato');
+const nomes = [];
+const numeros = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+
+});
+
+function adicionaLinha(){
+const inputNomeContato = document.getElementById('nome-contato');
+const inputNumeroContato = document.getElementById('numero-contato');
+
+if(nomes.includes(inputNomeContato.value)){
+    alert(`O contato: ${inputNomeContato.value} já foi inserido`);
+}else{
+    nomes.push(inputNomeContato.value);
+    numeros.push(parseInt(inputNumeroContato.value));
+    
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`
+    linha += `<td>${inputNumeroContato.value}</td>`
+    linha += `</tr>`
+    
+    linhas += linha;
+
+    console.log(linhas);
+}
+
+inputNomeContato.value = '';
+inputNumeroContato.value = '';
+
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+
+
